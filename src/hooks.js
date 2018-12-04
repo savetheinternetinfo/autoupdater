655b8fbe942d3a962a6ef19ac443d9c99b9f59b9
NullDev (Chris) <shadow@nulldev.org>
2018-12-04 22:22:35 +0100

    added pm2 manager and changed funcs to lambdas

--- src/hooks.js
+++ src/hooks.js
@@ -3,10 +3,17 @@
 // Core modules
 let exec = require("child_process").exec;
 
+// Dependencies
+let pm2 = require("pm2");
+
 // Utils
 let conf = require("./utils/configurator");
 let log = require("./utils/logger");
 let verifyRequest = require("./utils/verifyRequest");
+
+pm2.connect((err) => {
+    if (err) log.error(err);
+});
 
 /**
  * CLI command executor
@@ -17,12 +24,16 @@ let verifyRequest = require("./utils/verifyRequest");
 let executor = function(env, callback){
     let command = "cd " + config[env].repository_path;
     for (let nextCommand of config[env].commands) command += " && " + nextCommand;
-    exec(command, function(err, stdout, stderr){
+    exec(command, (err, stdout, stderr) => {
         callback(...arguments);
     });
 };
 
 let config = conf.getConfig();
+
+let pm2Handler = function(screiptName){
+
+};
 
 /**
  * Main hook function
@@ -35,7 +46,7 @@ module.exports = function(app){
         let response = verifyRequest(req, config.live.hook.secret);
         if (!response.valid) return log.error(response.error);
 
-        executor("live", function(err, stdout, stderr){
+        executor("live", (err, stdout, stderr) => {
             if (err || stderr){
                 if (err) log.error(`Exec Error on Live: ${err}`);
                 if (stderr) log.error(`Exec STDERR on Live: ${stderr}`);
@@ -51,7 +62,7 @@ module.exports = function(app){
         let response = verifyRequest(req, config.live.hook.secret);
         if (!response.valid) return log.error(response.error);
 
-        executor("dev", function(err, stdout, stderr){
+        executor("dev", (err, stdout, stderr) => {
             if (err || stderr){
                 if (err) log.error(`Exec Error on Dev: ${err}`);
                 if (stderr) log.error(`Exec STDERR on Dev: ${stderr}`);
