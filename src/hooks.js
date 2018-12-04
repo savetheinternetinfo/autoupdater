5f9f6633dc064fd5b5aac632a0cdc258930c36b5
NullDev (Chris) <shadow@nulldev.org>
2018-12-04 23:06:54 +0100

    I mean... technically this should work

--- src/hooks.js
+++ src/hooks.js
@@ -22,6 +22,8 @@ pm2.connect((err) => {
  * @param {*} callback
  */
 let executor = function(env, callback){
+    log.info(`Executing CLI commands for ${env}`);
+
     let command = "cd " + config[env].repository_path;
     for (let nextCommand of config[env].commands) command += " && " + nextCommand;
     exec(command, (err, stdout, stderr) => {
@@ -31,8 +33,26 @@ let executor = function(env, callback){
 
 let config = conf.getConfig();
 
-let pm2Handler = function(screiptName){
+/**
+ * Stop and start the node script
+ *
+ * @param {*} scriptName
+ * @param {*} env
+ * @param {*} scriptPath
+ */
+let pm2Handler = function(scriptName, env, scriptPath){
+    pm2.delete(scriptName, (delErr) => {
+        if (delErr) return log.error(`PM2 Delete Error for ${env} on script ${scriptName}: ${delErr}`);
 
+        let startOptions = {
+            name: scriptName,
+            script: scriptPath
+        };
+        pm2.start(startOptions, (startErr) => {
+            if (startErr) return log.error(`PM2 Start Error for ${env} on script ${scriptName} at ${scriptPath}: ${startErr}`);
+            return log.done(`Started script ${scriptName} on ${env} at ${scriptPath}`);
+        });
+    });
 };
 
 /**
@@ -43,33 +63,41 @@ let pm2Handler = function(screiptName){
 module.exports = function(app){
     // Live Deployment
     app.post(config.live.hook.path, (req, res) => {
+        log.info("Got build request for Live");
+
         let response = verifyRequest(req, config.live.hook.secret);
         if (!response.valid) return log.error(response.error);
 
+        log.done("Request for Live is valid!");
         executor("live", (err, stdout, stderr) => {
             if (err || stderr){
                 if (err) log.error(`Exec Error on Live: ${err}`);
                 if (stderr) log.error(`Exec STDERR on Live: ${stderr}`);
                 return;
             }
+            log.done("Executed CLI commands for Live successfully");
 
-            // TODO: PM2 STUFF
+            pm2Handler(config.live.pm2_process_name, "live", config.live.script_path);
         });
     });
 
     // Dev Deployment
     app.post(config.dev.hook.path, (req, res) => {
+        log.info("Got build request for Dev");
+
         let response = verifyRequest(req, config.live.hook.secret);
         if (!response.valid) return log.error(response.error);
 
+        log.done("Request for Dev is valid!");
         executor("dev", (err, stdout, stderr) => {
             if (err || stderr){
                 if (err) log.error(`Exec Error on Dev: ${err}`);
                 if (stderr) log.error(`Exec STDERR on Dev: ${stderr}`);
                 return;
             }
+            log.done("Executed CLI commands for Dev successfully");
 
-            // TODO: PM2 STUFF
+            pm2Handler(config.dev.pm2_process_name, "dev", config.dev.script_path);
         });
     });
 };
