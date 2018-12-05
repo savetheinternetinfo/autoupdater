d1a2342494f10f477abe18d9d8d749361d3c0f9e
NullDev (Chris) <shadow@nulldev.org>
2018-12-05 20:16:07 +0100

    if I rename a var I should do it right....

--- src/utils/verifyRequest.js
+++ src/utils/verifyRequest.js
@@ -44,24 +44,24 @@ let verifyRequest = function(req, secret){
 
     if (!req.headers["x-github-delivery"]){
         response.valid = false;
-        (response.error).push("No id found in the request");
+        (response.errors).push("No id found in the request");
     }
 
     if (!req.headers["x-github-event"]){
         response.valid = false;
-        (response.error).push("No event found in the request");
+        (response.errors).push("No event found in the request");
     }
 
     let sign = req.headers["x-hub-signature"] || "";
     if (!sign){
         response.valid = false;
-        (response.error).push("No signature found in the request");
+        (response.errors).push("No signature found in the request");
     }
 
     let reqData = JSON.stringify(req.body);
     if (!verifySignature(secret, reqData, sign)){
         response.valid = false;
-        (response.error).push("Failed to verify signature");
+        (response.errors).push("Failed to verify signature");
     }
 
     return response;
